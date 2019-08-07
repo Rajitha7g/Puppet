484f73bc543bf5e413c0a6de3234e08f0e9f52bf
student1 <student1@puppet.com>
2019-08-07 21:05:14 +0000

    Initial Commit

---
+++ manifests/init.pp
@@ -0,0 +1,21 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include time
+class time {
+  $servers = ['time.google.com']
+    case $::kernel {
+      'windows': {
+        class { 'winntp':
+          servers => $servers,
+        }
+    }
+    default: {
+      class { 'ntp':
+        servers => $servers,
+      }
+    }
+  }
+}
